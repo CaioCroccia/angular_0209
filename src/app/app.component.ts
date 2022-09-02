2cc90eea9df30e8514208c26b88c474ec1553c7a
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-02 20:27:43 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,14 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
-  nascimento = 2022;
+  name = 'Interpolação Angular App';
+  nascimento = 20;
  
   //O que é declarado dentro da classe é possivel usar no component hmtl
-}
 
+
+getIdade(){
+  return 2022 - this.nascimento;
+  };
+}
 
